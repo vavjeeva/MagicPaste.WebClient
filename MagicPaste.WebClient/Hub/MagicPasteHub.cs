545b3927b75421603afc17388c3ba1aeb866d8de
Jeeva Subburaj <vavjeeva@gmail.com>
2018-07-09 22:47:43 -0400

    Add project files.

---
+++ MagicPaste.WebClient/Hub/MagicPasteHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MagicPaste.WebClient
+{
+    public class MagicPasteHub : Hub
+    {
+        public async void SendData(string data)
+        {
+            await Clients.All.SendAsync("ReceiveData", data);
+        }
+    }
+}
---
+++ MagicPaste.WebClient/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/MagicPaste")
+    .build();
+
+connection.start();
+
+$("#btnPublish").click(function () {
+    var msg = $("#inputData").val();
+    if (msg.length > 0) {
+        connection.invoke("SendData", msg).catch(err => console.error(err.toString()));   
+        $("#inputData").val('');
+    }
+});
+
+connection.on("ReceiveData", (msg) => {
+    $("#messagesList").append($("<li class='list-group-item list-group-item-info'>").html(msg));
+});
